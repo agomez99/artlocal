374f1801b8aae63ca9d30e896de3a133a3a896b2
Austine Gomez <austine.gomez@yahoo.com>
2020-07-18 22:20:16 -0500

    no points

--- script.js
+++ script.js
@@ -421,19 +421,7 @@ if (!('remove' in Element.prototype)) {
   
     var popup = new mapboxgl.Popup({ closeOnClick: false })
       .setLngLat(currentFeature.geometry.coordinates)
-      .setHTML('<h3>Sweetgreen</h3>' +
-        '<h4>' + currentFeature.properties.address + '</h4>')
-      .addTo(map);
-  }
-  /**
-   * Create a Mapbox GL JS `Popup`.
-  **/
-  function createPopUp(currentFeature) {
-    var popUps = document.getElementsByClassName('mapboxgl-popup');
-    if (popUps[0]) popUps[0].remove();
-    var popup = new mapboxgl.Popup({closeOnClick: false})
-      .setLngLat(currentFeature.geometry.coordinates)
-      .setHTML('<h3>Sweetgreen</h3>' +
+      .setHTML('<h3>Art Locationj</h3>' +
         '<h4>' + currentFeature.properties.address + '</h4>')
       .addTo(map);
   }
